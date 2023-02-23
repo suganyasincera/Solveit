f3c1e6747ec3b749b9707858711bbd345ded52c7
jeyasuriyaprakash <115789315+jeyasuriyaprakash@users.noreply.github.com>
2023-02-23 17:44:18 +0530

    Add files via upload

--- Screens/Firstpage.js
+++ Screens/Firstpage.js
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet, Image, Dimensions, FlatList } from 'react-native'
-import React ,{useState} from 'react'
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { TouchableOpacity } from 'react-native';
 // import { useFonts } from 'expo-font';
@@ -9,52 +9,52 @@ import { FontAwesome } from '@expo/vector-icons';
 const windowWidth = Dimensions.get('window').width;
 
 const data = [
-    { id: 1, text: 'Slide 1' },
-    { id: 2, text: 'Slide 2' },
-    { id: 3, text: 'Slide 3' },
-    { id: 4, text: 'Slide 4' },
-  ];
+  { id: 1, text: 'Slide 1' },
+  { id: 2, text: 'Slide 2' },
+  { id: 3, text: 'Slide 3' },
+  { id: 4, text: 'Slide 4' },
+];
 
 
-  export default function Firstpage({navigation}) {
+export default function Firstpage({ navigation }) {
 
-    // const [loaded] = useFonts({
-    //   Montserrat: require('../assets/DM_Sans/DMSans-Regular.ttf'),
-    // });
+  // const [loaded] = useFonts({
+  //   Montserrat: require('../assets/DM_Sans/DMSans-Regular.ttf'),
+  // });
 
-    // if (!loaded) {
-    //   return null;
-    // }
- 
-    const [activeIndex, setActiveIndex] = useState(0);
-  
-    const handlePageChange = (newIndex) => {
-      setActiveIndex(newIndex);
-    };
+  // if (!loaded) {
+  //   return null;
+  // }
 
-    const renderSlide = ({ item }) => {
-        return (
-          <View style={styles.slide}>
-             <Image source={require('../assets/2img.png')} />
-             <Text  style={styles.slideText}>An IT Troubleshooting Service</Text>
-             <Text  style={styles.slideText1}>Lorem ipsum dolor sit amet, consectetur {'\n'}
-              adipiscing elit, sed do eiusmod tempor incididunt  {'\n'}
-               ut labore et dolore magna aliqua</Text>
-            {/* <Text style={styles.slideText}>{item.text}</Text> */}
-          </View>
-          
-        );
-      };
+  const [activeIndex, setActiveIndex] = useState(0);
 
+  const handlePageChange = (newIndex) => {
+    setActiveIndex(newIndex);
+  };
 
+  const renderSlide = ({ item }) => {
     return (
-        <View style={styles.container}>
-              <StatusBar style="dark" />
-              <View style={styles.fimg}>
-            <Image style={{resizeMode:'contain',width:'100%'}} source={require('../assets/1img.png')} />
-            </View>
+      <View style={styles.slide}>
+        <Image source={require('../assets/2img.png')} />
+        <Text style={styles.slideText}>An IT Troubleshooting Service</Text>
+        <Text style={styles.slideText1}>Lorem ipsum dolor sit amet, consectetur {'\n'}
+          adipiscing elit, sed do eiusmod tempor incididunt  {'\n'}
+          ut labore et dolore magna aliqua</Text>
+        {/* <Text style={styles.slideText}>{item.text}</Text> */}
+      </View>
 
-            <FlatList 
+    );
+  };
+
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="dark" />
+      <View style={styles.fimg}>
+        <Image style={{ resizeMode: 'contain', width: '100%' }} source={require('../assets/1img.png')} />
+      </View>
+
+      <FlatList
         data={data}
         horizontal
         showsHorizontalScrollIndicator={false}
@@ -64,9 +64,9 @@ const data = [
         onMomentumScrollEnd={(event) =>
           handlePageChange(Math.round(event.nativeEvent.contentOffset.x / windowWidth))
         }
-        
+
       />
-       <View style={styles.indicatorContainer}>
+      <View style={styles.indicatorContainer}>
         {data.map((_, index) => (
           <View
             key={index}
@@ -78,78 +78,78 @@ const data = [
         ))}
       </View>
 
-            <View style={{margin:30,flexDirection:'row',justifyContent:'space-between'}}>
-                <TouchableOpacity  onPress={() => navigation.navigate('Secondpage')}>
-                    <Text style={{fontSize:20,color:'#fff',fontWeight:'700'}}>Skip</Text>
-                </TouchableOpacity>
-                {/* <View style={{position:'relative',transform:[{rotate:'0deg'}]}}> */}
-                <TouchableOpacity 
-                style={{backgroundColor:'#fff',height:36,width:36,borderRadius:8,transform:[{rotate:'45deg'}],alignItems:'center',justifyContent:'center'}}
-                onPress={() => navigation.navigate('Secondpage')} >
-                    
-            <FontAwesome name="angle-double-right" size={25} color={'#FF9800'} style={{transform:[{rotate:'315deg'}]}} /> 
-          
-           
-                 </TouchableOpacity>
-                 {/* </View> */}
-            </View>
-            
-        </View>
-    )
+      <View style={{ margin: 30, flexDirection: 'row', justifyContent: 'space-between' }}>
+        <TouchableOpacity onPress={() => navigation.navigate('Secondpage')}>
+          <Text style={{ fontSize: 20, color: '#fff', fontWeight: '700' }}>Skip</Text>
+        </TouchableOpacity>
+        {/* <View style={{position:'relative',transform:[{rotate:'0deg'}]}}> */}
+        <TouchableOpacity
+          style={{ backgroundColor: '#fff', height: 36, width: 36, borderRadius: 8, transform: [{ rotate: '45deg' }], alignItems: 'center', justifyContent: 'center' }}
+          onPress={() => navigation.navigate('Secondpage')} >
+
+          <FontAwesome name="angle-double-right" size={25} color={'#FF9800'} style={{ transform: [{ rotate: '315deg' }] }} />
+
+
+        </TouchableOpacity>
+        {/* </View> */}
+      </View>
+
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#263A96',
-        // fontFamily:'DMSans-Regular'
-    
-       
-    },
-    fimg:{
-        justifyContent:'flex-start'
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#263A96',
+    // fontFamily:'DMSans-Regular'
 
-    slide: {
-        width: windowWidth,
-        height: 200,
-        justifyContent: 'center',
-        alignItems: 'center',
-        // backgroundColor: '#ccc',
-        marginTop:10
-      },
-      slideText: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: '#fff',
-        textAlign:'center',
-       
-        marginTop:10
-      },
-      slideText1:{
-        fontSize:12,
-        color:'#fff',
-        textAlign:'center',
-        marginTop:10,
-      },
 
-      indicatorContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        top:"-5%"
-        
-        
-      },
-      indicator: {
-        width: 10,
-        height: 10,
-        borderRadius: 5,
-        backgroundColor: '#ccc',
-        margin: 6,
-      },
-      activeIndicator: {
-        backgroundColor: '#FF9800',
-      },
+  },
+  fimg: {
+    justifyContent: 'flex-start'
+  },
+
+  slide: {
+    width: windowWidth,
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: '#ccc',
+    marginTop: 10
+  },
+  slideText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+
+    marginTop: 10
+  },
+  slideText1: {
+    fontSize: 12,
+    color: '#fff',
+    textAlign: 'center',
+    marginTop: 10,
+  },
+
+  indicatorContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    top: "-5%"
+
+
+  },
+  indicator: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: '#ccc',
+    margin: 6,
+  },
+  activeIndicator: {
+    backgroundColor: '#FF9800',
+  },
 });
 
 
--- Screens/Secondpage.js
+++ Screens/Secondpage.js
@@ -9,88 +9,90 @@ import { ScrollView } from 'react-native';
 
 const Separator = () => <View style={styles.separator} />;
 
-export default function Secondpage({navigation}) {
+export default function Secondpage({ navigation }) {
   return (
     <View style={styles.container}>
 
-      <Image style={{resizeMode:'contain',width:'100%',}} source={require('../assets/3img.png')} /> 
+      <Image style={{ resizeMode: 'contain', width: '100%', }} source={require('../assets/3img.png')} />
 
-      <Ionicons style={{margin: 5,
-    position: "absolute",
-    top: '5%',
-    left: 0,
-    width: 25,
-    height: 25,}} name='chevron-back-outline' size={30} color={'#263A96'}  onPress={() => navigation.navigate('Firstpage')} />
+      <Ionicons style={{
+        margin: 5,
+        position: "absolute",
+        top: '5%',
+        left: 0,
+        width: 25,
+        height: 25,
+      }} name='chevron-back-outline' size={30} color={'#263A96'} onPress={() => navigation.navigate('Firstpage')} />
 
       <ScrollView>
 
-      <View style={{ margin: 15 }}>
-        <Text style={{ color: '#292D3D', fontWeight: '700', fontSize: 20 }}>Welcome Back!</Text>
-        <Text style={{ color: '#919191', fontWeight: '500', fontSize: 16, marginTop: 10 }}>Hello there, Login to continue.</Text>
+        <View style={{ margin: 15 }}>
+          <Text style={{ color: '#292D3D', fontWeight: '700', fontSize: 20 }}>Welcome Back!</Text>
+          <Text style={{ color: '#919191', fontWeight: '500', fontSize: 16, marginTop: 10 }}>Hello there, Login to continue.</Text>
 
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-          <TouchableOpacity style={{ height: '100%', width: '45%', backgroundColor: '#fff', justifyContent: 'center', alignItems: 'center', marginTop: 16, padding: 0, borderRadius: 18, elevation: 5, shadowColor: '#2d2d2d', flexDirection: 'row' }} >
-            {/* <FontAwesome name="google" size={25} color={'#FF9800'} style={{marginRight:10}} /> */}
-            <Image source={require('../assets/search.png')} style={{ height: 25, width: 25, marginRight: 10 }} />
-            <Text style={{ textAlign: 'center', color: '#919191', fontSize: 14, fontWeight: '500' }}>Google</Text></TouchableOpacity>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <TouchableOpacity style={{ height: '100%', width: '45%', backgroundColor: '#fff', justifyContent: 'center', alignItems: 'center', marginTop: 16, padding: 0, borderRadius: 18, elevation: 5, shadowColor: '#2d2d2d', flexDirection: 'row' }} >
+              {/* <FontAwesome name="google" size={25} color={'#FF9800'} style={{marginRight:10}} /> */}
+              <Image source={require('../assets/search.png')} style={{ height: 25, width: 25, marginRight: 10 }} />
+              <Text style={{ textAlign: 'center', color: '#919191', fontSize: 14, fontWeight: '500' }}>Google</Text></TouchableOpacity>
 
-          <TouchableOpacity style={{ height: '100%', width: '45%', backgroundColor: '#263A96', justifyContent: 'center', alignItems: 'center', marginTop: 16, borderRadius: 18, elevation: 5, shadowColor: '#2d2d2d', flexDirection: 'row' }} >
-            <FontAwesome name="facebook" size={25} color={'#fff'} style={{ marginRight: 10 }} />
-            <Text style={{ textAlign: 'center', color: '#fff', fontSize: 14, fontWeight: '500' }}>Facebook</Text></TouchableOpacity>
+            <TouchableOpacity style={{ height: '100%', width: '45%', backgroundColor: '#263A96', justifyContent: 'center', alignItems: 'center', marginTop: 16, borderRadius: 18, elevation: 5, shadowColor: '#2d2d2d', flexDirection: 'row' }} >
+              <FontAwesome name="facebook" size={25} color={'#fff'} style={{ marginRight: 10 }} />
+              <Text style={{ textAlign: 'center', color: '#fff', fontSize: 14, fontWeight: '500' }}>Facebook</Text></TouchableOpacity>
+          </View>
         </View>
-      </View>
 
-      <View style={{ flexDirection: 'row', justifyContent: 'space-around', top: '2%', alignItems: 'center', margin: 10 }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around', top: '2%', alignItems: 'center', margin: 10 }}>
 
-        <Separator />
+          <Separator />
 
-        <Text style={{ color: '#919191', fontSize: 16, fontWeight: '500' }}>or sign in with</Text>
+          <Text style={{ color: '#919191', fontSize: 16, fontWeight: '500' }}>or sign in with</Text>
 
-        <Separator />
+          <Separator />
 
-      </View>
-
-      <View style={{ top: '0%', margin: 15 }}>
-        <Text style={styles.emailText}>Email / Phone number</Text>
-        <View style={styles.wrapperInput}>
-          <TextInput
-            style={styles.input}
-            placeholder="Email" />
-          <Ionicons name="checkmark-circle-outline" size={25} color={'#263A96'} style={{ marginRight: 10 }} />
         </View>
-      </View>
 
-      <View style={{ top: '0%', margin: 15 }}>
-        <Text style={styles.emailText}>Password</Text>
-        <View style={styles.wrapperInput}>
-          <TextInput
-            style={styles.input}
-            secureTextEntry={ true }
-            
-            placeholder="Password" />
-          {/* <Ionicons name="eye-outline" size={25} color={'#263A96'} style={{marginRight:10}} /> */}
-          <Ionicons name="eye-off-outline" size={25} color={'#919191'} style={{ marginRight: 10 }} />
+        <View style={{ top: '0%', margin: 15 }}>
+          <Text style={styles.emailText}>Email / Phone number</Text>
+          <View style={styles.wrapperInput}>
+            <TextInput
+              style={styles.input}
+              placeholder="Email" />
+            <Ionicons name="checkmark-circle-outline" size={25} color={'#263A96'} style={{ marginRight: 10 }} />
+          </View>
         </View>
-        <TouchableOpacity>
-        <Text style={{fontSize:12,fontWeight:'500',color:'#263A96',marginTop:6,textAlign:'right'}}>Forgot Password?</Text>
-        </TouchableOpacity>
-      </View>
+
+        <View style={{ top: '0%', margin: 15 }}>
+          <Text style={styles.emailText}>Password</Text>
+          <View style={styles.wrapperInput}>
+            <TextInput
+              style={styles.input}
+              secureTextEntry={true}
+
+              placeholder="Password" />
+            {/* <Ionicons name="eye-outline" size={25} color={'#263A96'} style={{marginRight:10}} /> */}
+            <Ionicons name="eye-off-outline" size={25} color={'#919191'} style={{ marginRight: 10 }} />
+          </View>
+          <TouchableOpacity>
+            <Text style={{ fontSize: 12, fontWeight: '500', color: '#263A96', marginTop: 6, textAlign: 'right' }}>Forgot Password?</Text>
+          </TouchableOpacity>
+        </View>
 
         <View>
-        <TouchableOpacity style={styles.button}  onPress={() => navigation.navigate('Thirdpage')}>
-              <Text style={{ color: '#fff', padding: 10, fontSize: 14, fontWeight:'500'  }}> Login Account </Text>
-            </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Thirdpage')}>
+            <Text style={{ color: '#fff', padding: 10, fontSize: 14, fontWeight: '500' }}> Login Account </Text>
+          </TouchableOpacity>
         </View>
 
-        <View style={{ flexDirection: 'row', justifyContent: 'center', marginBottom:50,margin:30}}>
-              <Text style={{ textAlign: 'center', color:'#919191',fontSize:12,fontWeight:'500' }}>Don't have an account?  </Text>
-              <TouchableOpacity style={{ alignItems: 'center' }}>
-                <Text style={{ color: '#263A96',fontSize:12,fontWeight:'500' }} > Sign Up</Text>
-              </TouchableOpacity>
-            </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'center', marginBottom: 50, margin: 30 }}>
+          <Text style={{ textAlign: 'center', color: '#919191', fontSize: 12, fontWeight: '500' }}>Don't have an account?  </Text>
+          <TouchableOpacity style={{ alignItems: 'center' }}>
+            <Text style={{ color: '#263A96', fontSize: 12, fontWeight: '500' }} > Sign Up</Text>
+          </TouchableOpacity>
+        </View>
 
 
-            </ScrollView>
+      </ScrollView>
 
 
 
@@ -143,16 +145,16 @@ const styles = StyleSheet.create({
     // borderWidth: 1,
     borderRadius: 25,
     elevation: 10,
-     shadowColor: '#2d2d2d',
+    shadowColor: '#2d2d2d',
     // color: '#fff',
     backgroundColor: '#263A96',
     marginTop: 10,
-    marginBottom:0,
+    marginBottom: 0,
     flexDirection: 'row',
 
     justifyContent: 'center',
     margin: 15,
-  
+
   },
 
 });
